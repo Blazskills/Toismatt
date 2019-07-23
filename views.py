89a04e47abd6544a20826577ad2f98c2a411b451
Temitope <ilesanmiisaac@gmail.com>
2019-07-23 11:33:32 +0100

    password testing

--- views.py
+++ views.py
@@ -3,6 +3,7 @@ from flask_login import LoginManager,UserMixin,login_user, logout_user, login_re
 from test import db
 from flask import Flask,redirect, url_for, render_template, request, flash
 from models import Regtb
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from werkzeug.security import generate_password_hash, check_password_hash
 
 @app.route('/')
@@ -75,27 +76,42 @@ def reset():
              Email=request.form['Email']
              user=Regtb.query.filter_by(Email=Email).first()
              if user:
+                   code = str(id.uuid4())
+                   user.change_configuration={"password_reset_code":code}
+                   user.save()
+
+                   #emsil the user
+                   body_html = render_template('mail/user/password_reset.html', user =user)
+                   body_text = render_template('mail/user/password_reset.html', user =user)
+                   Email(user.Email,"password reset request", body_html, body_text)
+                   #remove later
                    flash('There is an account with that email. you can now change your password','success')
-                   return render_template('resetpassword.html')   
+                   return render_template('index.html')   
              flash('There is no account with that email. You must register first','danger')
              return render_template('reset.html')    
       return render_template('reset.html')
 
 
-@app.route('/resetpassword',methods=['GET','POST'])
-# @login_required
-def resetpassword():
-       if reset():
-              if request.method == 'POST':
-                     Password = request.form['Password']
-                     Confirm_Password = request.form['Confirm_Password']
-                     secure_password = generate_password_hash(Password, method="sha256")
-                     if Password == Confirm_Password:
-                           return render_template('index.html')
-              flash('something went wrong','danger')
-              return render_template('index.html')   
+# @app.route('/resetpassword',methods=['GET','POST'])
+# # @login_required
+# def send_password_reset_email(user_email):
+#        password_reset_serializer = Serializer(app.config['SECRET_KEY'])
+#        password_reset_url = url_for('users.reset_with_token',token = password_reset_serializer.dumps(user_email, salt='password-reset-salt'),_external=True)
+#        html = render_template( 'email_password_reset.html', password_reset_url=password_reset_url)
+#       send_email('Password Reset Requested', [user_email], html)
+      #  if reset():
+      #         if request.method == 'POST':
+      #                Password = request.form['Password']
+      #                Confirm_Password = request.form['Confirm_Password']
+      #                secure_password = generate_password_hash(Password, method="sha256")
+      #                if Password == Confirm_Password:
+      #                      return render_template('index.html')
+      #         flash('something went wrong','danger')
+      #         return render_template('index.html')   
 
-       return render_template('resetpassword.html')
+      #  return render_template('resetpassword.html')
+
+
 #   @app.route('/reset',methods=['GET','POST'])
 #   def reset():
 #    if request.method == 'POST':
